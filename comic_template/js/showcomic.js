d1ddca21cc2ce0dbc6d7b9a29ab16ebde7ffb14d
Jaden Eckel <jadeneckel@gmail.com>
2021-03-02 14:32:10 -0500

    nav on both top and bottom

--- comic_template/js/showcomic.js
+++ comic_template/js/showcomic.js
@@ -25,56 +25,62 @@ let ext = "png"; //file extension of your comic pages
 let path = (folder != "" ? folder + "/" : "") + image + pg + "." + ext; //string a bunch of those variables together to create a path to your current comic page
 
 //ACTUALLY SHOWING THE COMICS AND NAV ON THE PAGE
+writeNav(); //show navigation at the top of the comic
+
 if (pg < maxpg) { //check whether comic is on the last page
   document.write(`<a href="?pg=` + (pg + 1) + `"/><img src="` + path + `"/></a><br/>`); //display comic page and make it so that clicking it will lead you to the next page
 } else {
   document.write(`<img src="` + path + `"/><br/>`); //display comic page without link
-}
+};
+
+writeNav(); //show navigation at the bottom of the comic
 
 //NAV
-//FIRST BUTTON
-if (pg > 2) { //wait until page 3 to make button active
-document.write(`<a href="?pg=` + 1 + `"/>First</a>`);
-} else {
-document.write(`First`);
-}
+function writeNav() { //this is a function that writes both the top and bottom nav buttons
+  //FIRST BUTTON
+  if (pg > 2) { //wait until page 3 to make button active
+  document.write(`<a href="?pg=` + 1 + `"/>First</a>`);
+  } else {
+  document.write(`First`);
+  }
 
-document.write(` | `); //divider
+  document.write(` | `); //divider
 
-//PREV BUTTON
-if (pg > 1) { //wait until page 2 to make active
-document.write(`<a href="?pg=` + (pg - 1) + `"/>Prev</a>`);
-} else {
-document.write(`Prev`);
-}
+  //PREV BUTTON
+  if (pg > 1) { //wait until page 2 to make active
+  document.write(`<a href="?pg=` + (pg - 1) + `"/>Prev</a>`);
+  } else {
+  document.write(`Prev`);
+  }
 
-document.write(` | `); //divider
+  document.write(` | `); //divider
 
-//NEXT BUTTON
-if (pg < maxpg) { //only make active if not on the last page
-  document.write(`<a href="?pg=` + (pg + 1) + `"/>Next</a>`);
-} else {
-document.write(`Next`);
-}
+  //NEXT BUTTON
+  if (pg < maxpg) { //only make active if not on the last page
+    document.write(`<a href="?pg=` + (pg + 1) + `"/>Next</a>`);
+  } else {
+  document.write(`Next`);
+  }
 
-document.write(` | `);
+  document.write(` | `);
 
-//LAST BUTTON
-if (pg < maxpg - 1) { //only make active if not on last page
-document.write(`<a href="?pg=` + maxpg + `"/>Last</a>`);
-} else {
-document.write(`Last`);
+  //LAST BUTTON
+  if (pg < maxpg - 1) { //only make active if not on last page
+  document.write(`<a href="?pg=` + maxpg + `"/>Last</a> <br />`);
+  } else {
+  document.write(`Last <br />`); //note that line breaks are added after each document.write, mainly to correctly place the top nav
+  }
 }
 
 //KEYBOARD NAVIGATION
 document.addEventListener("keydown", (e) => {
-  if ((e.key == 'ArrowRight' || e.key.toLowerCase() == 'd') && pg < maxpg) { //USE RIGHT ARROW OR D TO GO TO NEXT PAGE
+  if ((e.key == 'ArrowRight' || e.key.toLowerCase() == 'd') && pg < maxpg) { //right arrow or D goes to next page
     window.location.href = "?pg=" + (pg + 1);
-  } else if ((e.key == "ArrowLeft" || e.key.toLowerCase() == "a") && pg > 1) { //USE LEFT ARROW OR A TO GO TO PREVIOUS PAGE
+  } else if ((e.key == "ArrowLeft" || e.key.toLowerCase() == "a") && pg > 1) { //left arrow or A goes to previous page
     window.location.href = "?pg=" + (pg - 1);
-  } else if (e.key.toLowerCase() == "w") { //USE W TO SCROLL UP
+  } else if (e.key.toLowerCase() == "w") { //W scrolls up
     window.scrollBy({ top: -30 });
-  } else if (e.key.toLowerCase() == "s") { //USE S TO SCROLL DOWN
+  } else if (e.key.toLowerCase() == "s") { //S scrolls down
     window.scrollBy({ top: 30 });
   }
 });
