abe45769b48adb41ba8bbd1c029e8d7e554e3281
Jaden Eckel <jadeneckel@gmail.com>
2021-03-26 03:19:09 -0400

    made mobile friendly, styled top navbar

--- src/comic_template/js/comic_header.js
+++ src/comic_template/js/comic_header.js
@@ -1,11 +1,13 @@
-//the header of the site would be handled in this javascript file, so you don't have to copypaste it onto every page
-
+//the header of the site would be handled in this javascript file, so you don't have to copypaste it onto every page.
+//There are much better methods of handling the header out there but you can stick with this one if you want
 document.write(`
     <header>
-        <img src="./img/logo.png" width=500 alt="" /> 
+        <a href="home.html"><img src="./img/logo.png" alt="" /></a> 
 
         <div id="nav">
-            <a href="archive.html">ARCHIVE</a>
+            <a href="home.html">HOME</a> |
+            <a href="archive.html">ARCHIVE</a> |
+            <a href="characters.html">CHARACTERS</a>
         </div>
     </header>
 `);
